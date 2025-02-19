726d0265fdc0b1f5c4964407333905ba57632ec0
mgclaflin <mgclaflin@crimson.ua.edu>
2025-02-19 17:29:05 +0100

    added logging and error handling functionality to extract.py script

--- scripts/extract.py
+++ scripts/extract.py
@@ -9,13 +9,18 @@ from scripts.config import ENV_PATH, CITIES_CONFIG_PATH, RAW_DATA_PATH, RAW_COMP
 
 # loading environment variables (API key)
 def load_env_api():
-    
-    # load envrionment variables from the .env file
-    load_dotenv(ENV_PATH)
-
-    # get the API key from the .env file
-    api_key = os.getenv("API_KEY")
-    return api_key
+    try:
+        # load envrionment variables from the .env file
+        load_dotenv(ENV_PATH)
+        # get the API key from the .env file
+        api_key = os.getenv("API_KEY")
+        if not api_key:
+            raise ValueError("API_KEY is missing in the env variables")
+        logger.info("Successfully loaded API key")
+        return api_key
+    except Exception as e:
+        logger.error(f"Error loading API key: {e}")
+        raise
 
 # function to execute the API call
 # api call to get the current weather 
@@ -35,27 +40,35 @@ def get_weather(api_key, city, lat, lon, exclude='minutely,daily,hourly', units=
     # Add the 'exclude' parameter if it's provided
     if exclude:
         params['exclude'] = exclude
+        
     try:
         # Make the API request
         response = requests.get(api_url, params=params)
-
+        response.raise_for_status()
         # Check if the request was successful
         data = response.json()
         # Print or process the data
         data['City']=city
+        logger.info(f"Successfully fetched weather data for {city}")
         print(data)
-        logger.info(f"Successfully fetched data from {api_url}")
         return data
     except requests.exceptions.RequestException as e:
         print(f"Error: {response.status_code}, {response.text}")
         logger.error(f"Error fetching data from {api_url}: {e}")
-        return None
+    except json.JSONDecodeError as e:
+        logger.error(f"Failed to decode JSON response for {city}: {e}")
+    return None
+    
 
 # function to run the API call based upon cities in config file
 def city_weather_data_extraction():
-    # Step 1: Load the config file
-    with open(CITIES_CONFIG_PATH, 'r') as f:
-        config_data = json.load(f)
+    try:
+        # Step 1: Load the config file
+        with open(CITIES_CONFIG_PATH, 'r') as f:
+            config_data = json.load(f)
+    except (IOError, json.JSONDecodeError) as e:
+        logger.error(f"Error loading cities config file: {e}")
+        raise
 
     weather_data = []
 
@@ -65,6 +78,10 @@ def city_weather_data_extraction():
         latitude = city['latitude']
         longitude = city['longitude']
         city_name = city['name']
+        
+        if not all([city_name,latitude,longitude]):
+            logger.warning(f"skipping city due to missing data: {city}")
+            continue
 
         weather_data.append(get_weather(api_key, city_name, latitude, longitude))
         
@@ -73,44 +90,58 @@ def city_weather_data_extraction():
 # writing the extracted data to the raw_weather_data.json file
 def write_raw_data(weather_data):
 
-    # Check if the file exists to decide whether to append or create new
-    if os.path.exists(RAW_DATA_PATH):
-        # If the file exists, load the existing data, then append new data
-        with open(RAW_DATA_PATH, 'r') as json_file:
-            existing_data = json.load(json_file)
-            existing_data.extend(weather_data)
+    try:
+        # Check if the file exists to decide whether to append or create new
+        if os.path.exists(RAW_DATA_PATH):
+            # If the file exists, load the existing data, then append new data
+            with open(RAW_DATA_PATH, 'r') as json_file:
+                existing_data = json.load(json_file)
+                existing_data.extend(weather_data)
 
-        # Append to the file
-        with open(RAW_DATA_PATH, 'w') as json_file:
-            json.dump(existing_data, json_file, indent=4)
-    else:
-        # If the file doesn't exist, create it and write the new data
-        with open(RAW_DATA_PATH, 'w') as json_file:
-            json.dump(weather_data, json_file, indent=4)
+            # Append to the file
+            with open(RAW_DATA_PATH, 'w') as json_file:
+                json.dump(existing_data, json_file, indent=4)
+        else:
+            # If the file doesn't exist, create it and write the new data
+            with open(RAW_DATA_PATH, 'w') as json_file:
+                json.dump(weather_data, json_file, indent=4)
 
-    print(f"Data saved to {RAW_DATA_PATH}")
+        print(f"Data saved to {RAW_DATA_PATH}")
+        logger.info(f"Weather data successfully saved to {RAW_DATA_PATH}")
+    except (IOError, json.JSONDecodeError, ValueError) as e:
+        logger.error(f"Error writing to {RAW_FILE_PATH}: {e}")
+        raise
     
 # writing the extracted data to the raw_compiled_data.json file
 def write_compiled_raw_data(weather_data):
+    
+    try:
+        # Check if the file exists to decide whether to append or create new
+        if os.path.exists(RAW_COMPILED_PATH):
+            # If the file exists, load the existing data, then append new data
+            with open(RAW_COMPILED_PATH, 'r') as json_file:
+                existing_data = json.load(json_file)
+                existing_data.extend(weather_data)
 
-    # Check if the file exists to decide whether to append or create new
-    if os.path.exists(RAW_COMPILED_PATH):
-        # If the file exists, load the existing data, then append new data
-        with open(RAW_COMPILED_PATH, 'r') as json_file:
-            existing_data = json.load(json_file)
-            existing_data.extend(weather_data)
+            # Append to the file
+            with open(RAW_COMPILED_PATH, 'w') as json_file:
+                json.dump(existing_data, json_file, indent=4)
+        else:
+            # If the file doesn't exist, create it and write the new data
+            with open(RAW_COMPILED_PATH, 'w') as json_file:
+                json.dump(weather_data, json_file, indent=4)
 
-        # Append to the file
-        with open(RAW_COMPILED_PATH, 'w') as json_file:
-            json.dump(existing_data, json_file, indent=4)
-    else:
-        # If the file doesn't exist, create it and write the new data
-        with open(RAW_COMPILED_PATH, 'w') as json_file:
-            json.dump(weather_data, json_file, indent=4)
+        print(f"Data saved to {RAW_COMPILED_PATH}")
+        logger.info(f"Weather data successfully saved to {RAW_COMPILED_PATH}")
+    except (IOError, json.JSONDecodeError, ValueError) as e:
+        logger.error(f"Error writing to {RAW_COMPILED_PATH}: {e}")
+        raise
 
-    print(f"Data saved to {RAW_COMPILED_PATH}")
 
-# executing the api calls & writing to the file functions
-weather_data = city_weather_data_extraction()
-write_raw_data(weather_data)
-write_compiled_raw_data(weather_data)
+try:
+    # executing the api calls & writing to the file functions
+    weather_data = city_weather_data_extraction()
+    write_raw_data(weather_data)
+    write_compiled_raw_data(weather_data)
+except Exception as e:
+    logger.critical(f"Pipeline extraction failed at extract.py: {e}")
